9e30884e42469f2a0f4a3778996c323f584f0094
Fangxia Ding <129153265+Fangxia2023@users.noreply.github.com>
2024-08-04 16:50:28 -0700

    UI change for achievement page

--- lib/commitment_time/Achievement.dart
+++ lib/commitment_time/Achievement.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:bfootlearn/components/custom_appbar.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CongratulationPage extends StatelessWidget {
   final String message;
-
   const CongratulationPage({Key? key, required this.message}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: customAppBar(context: context, title: 'Learning Goal'),
+      appBar: customAppBar(context: context, title: 'Achievement'),
       body: Stack(
         children: [
           Center(
@@ -19,41 +20,61 @@ class CongratulationPage extends StatelessWidget {
               children: [
                 Image.asset(
                   'assets/Achivement_page.png',
-                  // 设置图片的高度和宽度，或者使用alignment属性进行控制。
                   height: 250,
                   width: 250,
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'Congratulations!',
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                ScaleAnimatedTextKit(
+                  text: ["Congratulations!", "You did it!"],
+                  textStyle: TextStyle(
+                    fontSize: 35,
+                    fontFamily: 'Chewy',
+                    foreground: Paint()
+                      ..shader = LinearGradient(
+                        colors: <Color>[
+                          Color(0xffbdbcfd),
+                          Color.fromARGB(255, 175, 145, 230),
+                        ],
+                      ).createShader(Rect.fromLTWH(0.0, 0.0, 400.0, 70.0)),
                   ),
                   textAlign: TextAlign.center,
+                  totalRepeatCount: 100, // 无限循环的近似值
                 ),
-                SizedBox(height: 50),
+                SizedBox(height: 30),
                 Text(
                   'You got an achievement!',
                   style: TextStyle(
-                    fontSize: 24.0,
-                    color: Colors.black,
+                    fontSize: 35,
+                    fontFamily: 'Chewy',
+                    foreground: Paint()
+                      ..shader = LinearGradient(
+                        colors: <Color>[
+                          Color(0xffbdbcfd),
+                          Color.fromARGB(255, 175, 145, 230),
+                        ],
+                      ).createShader(Rect.fromLTWH(0.0, 0.0, 400.0, 70.0)),
                   ),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 50),
-                ElevatedButton(
+                SizedBox(height: 100),
+                ElevatedButton.icon(
                   onPressed: () {
-                    // 可以添加分享功能
+                    //Navigator.of(context).pop(); // 关闭界面
                   },
-                  child: Text('Share'),
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.purple,
-                    textStyle: TextStyle(
+                  icon: Icon(Icons.share, color: Colors.white),
+                  label: const Text(
+                    'Share',
+                    style: TextStyle(
                       fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
                     ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 20),
+                    elevation: 10,
+                    shadowColor: Color.fromARGB(255, 58, 13, 79),
+                    backgroundColor: Color.fromARGB(255, 175, 145, 230),
                   ),
                 ),
               ],
@@ -61,7 +82,7 @@ class CongratulationPage extends StatelessWidget {
           ),
           Positioned.fill(
             child: Lottie.asset(
-              'assets/confetti.json', // 替换成实际的文件路径
+              'assets/confetti.json', // Replace with actual file path
               fit: BoxFit.cover,
             ),
           ),
